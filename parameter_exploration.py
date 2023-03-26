7b236866e29bcf1b136d7231d275faa38e628d5a
Theo N. Dionne <theo.nathaniel.dionne@usherbrooke.ca>
2023-03-26 00:59:20 -0400

    I don't even know

--- parameter_exploration.py
+++ parameter_exploration.py
@@ -15,24 +15,21 @@ CYCLES = 20000
 
 # parameter values
 B_VALUES = np.array([0.1,0.5,1,5,10])
-MAX_ALPHA_VALUES = np.array([0.5])
+MAX_ALPHA_VALUES = np.array([0.5,1,2])
 D_X_VALUES = np.array([1])
 X_0_VALUES = np.array([1])
 
 # filename
-OUTPUT_DIR = "gaming_as_hell_half"
+OUTPUT_DIR = "debug_test"
 
-""" PROCEDURE """
-
-# initialize simulation/é
-Simulation = icef.SnowflakeSimulation(L) 
+""" PROCEDURE """ 
 
 # simulation info
 simulation_counter = 0
 
 # Open our existing CSV file in append mode
 # Create a file object for this file
-with open(f"{OUTPUT_DIR}/specs.csv", 'a') as f_object:
+with open(f"{OUTPUT_DIR}/specs.csv", 'w+') as f_object:
 
     # Pass this file object to csv.writer()
     # and get a writer object
@@ -40,7 +37,7 @@ with open(f"{OUTPUT_DIR}/specs.csv", 'a') as f_object:
 
     # Pass the list as an argument into
     # the writerow()
-    writer_object.writerow(["Simulation number", "b", "max_alpha", "D_x", "X_0", f"Cycles={CYCLES}"])
+    writer_object.writerow(["Simulation number", "b", "max_alpha", "D_x", "X_0", "global_time", f"Cycles={CYCLES}", f"L={L}"])
 
     # Close the file object
     f_object.close()
@@ -49,14 +46,17 @@ for b in B_VALUES:
     for max_alpha in MAX_ALPHA_VALUES:
         for D_x in D_X_VALUES:
             for X_0 in X_0_VALUES:
+                # initialize simulation
+                Simulation = icef.SnowflakeSimulation(L) # I have no clue why this wasn't working
+
                 #increment counter
                 simulation_counter += 1
 
                 # simulation parameters
-                Simulation.PhysicsU.X_0 = 1
-                Simulation.PhysicsU.max_alpha = 1
-                Simulation.PhysicsU.b = 1
-                Simulation.PhysicsU.D_x = 1
+                Simulation.PhysicsU.X_0 = X_0
+                Simulation.PhysicsU.max_alpha = max_alpha
+                Simulation.PhysicsU.b = b
+                Simulation.PhysicsU.D_x = D_x
 
                 # console update
                 print(f"Simulating --- b={b} ; max_alpha={max_alpha} ; D_x={D_x} ; X_0={X_0}")
@@ -64,8 +64,10 @@ for b in B_VALUES:
                 ### actually simulating ###
                 final_ice_map = Simulation.run_simulation(CYCLES)
                 
+
+
                 # params that we want to add as a new row
-                List = [simulation_counter, b, max_alpha, D_x, X_0]
+                List = [simulation_counter, b, max_alpha, D_x, X_0, Simulation.global_time]
                 
                 # Open our existing CSV file in append mode
                 # Create a file object for this file
